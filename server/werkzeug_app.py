639838d0c8bbe23876f81e06d981abf5781f7fc2
hesbonmanyi254 <hesbonmanyinsa96@gmail.com>
2024-01-31 13:31:41 +0300

    completed

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    # This line prints a message to the terminal when a request is received
+    print(f'This web server is running at {request.remote_addr}')
+    
+    # This line sends the response message to the client's browser
+    return Response('A WSGI generated this response!')
+
+# Check if the script is being run directly
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    
+    # Start the development server
+    run_simple(
+        hostname='localhost',
+        port=5555,
+        application=application
+    )
